54fbfb44193d939faacb985860d76bad11020de7
James Alhassan <jamesalhassan052@gmail.com>
2023-07-27 05:47:56 +0000

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -29,7 +29,7 @@ class MRUCache(BaseCaching):
     def __init__(self):
         'Initializes the cache.'
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.cache_data = OD()
 
     def put(self, key, item):
         'Adds an item in the cache'
