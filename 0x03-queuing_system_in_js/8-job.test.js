5975f306071916a63c988b3f86be0af64408b582
James Alhassan <jamesalhassan052@gmail.com>
2023-09-01 12:17:20 +0000

    Add 8-job.test.js

---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,17 @@
+import createPushNotificationsJobs from './8-job';
+import kue from 'kue';
+import { expect } from 'chai';
+
+const queue = kue.createQueue();
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account',
+  },
+  {
+    phoneNumber: '4153118782',
+    message: 'This is the code 4321 to verify your account',
+  },
+];
+
